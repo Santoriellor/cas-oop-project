1ee80fed4f9889d2c1ad2a25753a69c6a2049b53
Andreas Zetzsche <andreaszetzsche31@gmail.com>
2026-01-05 20:40:59 +0100

    Course-documents geändert

---
+++ frontend/src/app/course-documents/course-documents.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DocumentService} from "../service/document.service";
+import { CourseDocumentDto} from "../model/document.models";
+
+
+@Component({
+  selector: 'app-course-documents',
+  templateUrl: './course-documents.html',
+})
+export class CourseDocumentsComponent implements OnInit {
+
+  courseId!: number;
+  documents: CourseDocumentDto[] = [];
+  loading = false;
+  error: string | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private documentService: DocumentService
+  ) {}
+
+ngOnInit(): void {
+    this.courseId = Number(this.route.snapshot.paramMap.get('courseId'));
+    this.loadDocuments();
+}
+
+loadDocuments(): void {
+    this.loading = true;
+    this.error = null;
+
+    this.documentService.getDocumentsByCourse(this.courseId).subscribe({
+      next: docs => {
+        this.documents = docs;
+        this.loading = false;
+      },
+    error: err => {
+        this.loading = false;
+        if(err.status === 403) {
+          this.error = 'Du bist für diesen Kurs nicht eingeschrieben.'
+        } else {
+          this.error = 'Dokument konnten nicht geladen werden.'
+        }
+    }
+  });
+}
+}
