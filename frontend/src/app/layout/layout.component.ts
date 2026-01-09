485131f1be65b75df5d1253a2b2d06d56b31b5ee
santorem <santorem@students.zhaw.ch>
2026-01-09 17:12:18 +0100

    hide menu update

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -16,7 +16,7 @@ import { AuthService } from '../service/auth.service';
               </span>
             </a>
 
-            <button class="icon-btn" (click)="toggleSidebar()" aria-label="Menü öffnen">
+            <button class="icon-btn md:hidden" (click)="toggleSidebar()" aria-label="Menü öffnen">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
